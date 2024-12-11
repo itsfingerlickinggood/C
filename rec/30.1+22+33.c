0cf7d83e6e626f6e12085dd8043c27f851155374
itsfingerlickinggood <sonuipad05@gmail.com>
2024-12-11 23:44:45 +0530

    3.8

---
+++ rec/30.1+22+33.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter the number to find the pattern of (1 + 2/2! + 3/3!) : ");
+	scanf("%d",&a);
+	
+	int i,j;
+	float f1actorial,sum=0;
+	
+	for(i=1;i<=a;i++){
+		factorial=1;
+		for(j=1;j<=i;j++){
+			factorial*=j;
+		}
+		sum+=i/factorial;
+	}
+	
+	printf("The sum of the sequence of (1 + 2/2! + 3/3!) is %.3f:",sum);
+	
+	return 0;	
+}
