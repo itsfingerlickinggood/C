1640e4be02194c2c8d485a0768aa479e42d7155c
itsfingerlickinggood <sonuipad05@gmail.com>
2024-12-20 22:25:28 +0530

    4.8

--- rec/52.uplow-triangluar-matrix.c
+++ rec/52.uplow-triangluar-matrix.c
@@ -1,36 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int matrix[10][10], n, isSame = 1,i,j;
-
-    // Input matrix size
-    printf("Enter the size of the matrix (n): ");
+    int i,n, matrix[100][100], symmetric = 1,j;
     scanf("%d", &n);
 
-    // Input matrix elements
-    printf("Enter the elements of the matrix:\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
         for (j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
+            if (i < j && matrix[i][j] != matrix[j][i])
+                symmetric = 0;
         }
-    }
 
-    // Check if upper triangular is same as lower triangular
-    for (i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j++) {
-            if (matrix[i][j] != matrix[j][i]) {
-                isSame = 0;
-                break;
-            }
-        }
-    }
-
-    // Output result
-    if (isSame)
-        printf("Upper triangular is the same as lower triangular.\n");
-    else
-        printf("Upper triangular is NOT the same as lower triangular.\n");
-
+    printf(symmetric ? "Symmetric\n" : "Not Symmetric\n");
     return 0;
 }
 
