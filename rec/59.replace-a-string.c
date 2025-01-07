1a2a51f8ac35e59cb13634777d9c4157212c43bc
itsfingerlickinggood <sonuipad05@gmail.com>
2025-01-07 08:02:43 +0530

    5.2

--- rec/59.replace-a-string.c
+++ rec/59.replace-a-string.c
@@ -2,22 +2,31 @@
 int main(){
 	char a[20],b,c;
 	
-	printf("Enter a string");
+	printf("Enter a string : ");
 	scanf("%s",a);
 	
 	printf("Enter the char to be replaced : ");
-	scanf("%s",b);
+	scanf(" %c",&b);
 	
-	int i;
+	int i,x=1;
 	
 	for(i=0;a[i]!='\0';i++){
 		if(a[i]==b){
-			printf("Enter the char to replace %s : ",b);
-			scanf("%s",c);
-			
-		}
-		else{
-			printf("Character is NOT Found !");
+			printf("Character Found !\n");
+			printf("Enter the char to replace %c : ",b);
+			scanf("  %c",&c);
+			printf("\n");
+			a[i]=c;
+			x=0;
 		}
 	}
+	
+	if(x){
+		printf("\nCharacter is NOT Found !");
+	}
+	else{
+		printf("Character is Replaced Successfully and Repalced string is : %s !",a);
+	}
+	
+	return 0;
 }
