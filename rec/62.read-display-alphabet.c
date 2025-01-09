9d4148e8e456ddc1f20ef92b4c3d7961dce327f1
itsfingerlickinggood <sonuipad05@gmail.com>
2025-01-09 08:47:48 +0530

    5.6

--- rec/62.read-display-alphabet.c
+++ rec/62.read-display-alphabet.c
@@ -2,43 +2,32 @@
 #include<string.h>
 
 int main(){
+	int size;
+	printf("Enter the no of names : ");
+	scanf("%d",&size);
 	
-	char ans='y';
+	char names[size][100],temp[100];
 	
-	while(ans=='y'){
-		
-		int size;
-		
-		printf("Enter the no of names : ");
-		scanf("%d",&size);
-		
-		char names[size][100],temp[100];
-		
-		int i,j;
-		
-		for(i=0;i<size;i++){
-			printf("Enter name %d : ");
-			scanf("%s",names[i]);
+	int i,j;
+	
+	for(i=0;i<size;i++){
+		printf("Enter name %d : ",i+1);
+		scanf("%s",names[i]);
+	}
+	
+	for(i=0;i<size;i++){
+		for(j=0;j<i;j++){
+			if(strcmp(names[i],names[j])<0){
+				strcpy(temp,names[i]);
+				strcpy(names[i],names[j]);
+				strcpy(names[j],temp);
+			}
 		}
-		
-		 for (i = 0; i < a - 1; i++) {
-        for (j = i + 1; j < a; j++) {
-            if (strcmp(names[i], names[j]) > 0) { // Compare and swap
-                strcpy(temp, names[i]);
-                strcpy(names[i], names[j]);
-                strcpy(names[j], temp);
-            }
-        }
-    }
-    
-    printf("\nNames in alphabetical order:\n");
-    for (i = 0; i < a; i++) {
-        printf("%s\n", names[i]);
-    }
-    
-		
-		printf("Do you want to continue (y/n) : ");
-		scanf("%c",&ans);
+	}
+	
+	printf("The names in alphabetical order is : \n");
+	for(i=0;i<size;i++){
+		printf("%s\n",names[i]);
 	}
 	
 	return 0;
